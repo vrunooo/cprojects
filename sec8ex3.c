a3a9813ad0d0dc1f59db622f29c39bac5fe36300
Bruno Polaski <116308830+vrunooo@users.noreply.github.com>
2023-02-27 21:24:21 -0300

    Create sec8ex3.c

---
+++ sec8ex3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int vetor[10];
+	for(int i = 0; i < 10; i++){
+		printf("Coloque um numero para o vetor: ");
+		scanf("%d", &vetor[i]);
+	}
+	for(int i = 9; i >= 0; i--){
+		printf("%d\n", vetor[i]);
+	}
+}
